0ac8a8f8c87e74daa11d82cb9327eb1797d180c1
Paolo Miguel Pugeda <ajoseh8@gmail.com>
2026-05-11 15:22:48 +0800

    Add clickable  function to view certificate

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const mainMenu = document.getElementById("menu");
 burg.addEventListener("click", () => {
   mainMenu.classList.toggle("hidden");
 });
+
+
+
+const modal = document.getElementById("imageModal");
+            const modalImg = document.getElementById("modalImg");
+
+            function openModal(src) {
+            modal.classList.remove("hidden");
+            modalImg.src = src;
+            }
+
+            modal.addEventListener("click", () => {
+            modal.classList.add("hidden");
+            });
